628e1f55f2d97fead6b94deef540135f6906145f
iroelst <iroel.setyawan@gmail.com>
2026-02-22 20:36:13 +0700

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'melona-v1';
+const assets = [
+  './',
+  './index.html',
+  './melona_logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+// Fetching assets
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(response => {
+      return response || fetch(e.request);
+    })
+  );
+});
